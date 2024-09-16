d0f6901a48114e88a48729b5390e1765ab1f9c1e
goldiefever <mgoldhaberfiebert2028@kehillahstudent.org>
2024-09-16 09:06:38 -0700

    finished w/ a02

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import turtle
+
+turtle.shape("turtle")
+
+f=turtle.forward
+r=turtle.right
+rightangle=90
+
+r(rightangle)
+
+for spiralside in range(1,7):
+    f(spiralside*50)
+    r(rightangle)
+    f(spiralside*50)
+    r(rightangle)
+
+turtle.exitonclick()
+
